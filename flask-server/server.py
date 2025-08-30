a47ef952f732383d9d6e87c4ff8953ae9b3e97e2
Johnny <panghowu@gmail.com>
2025-08-30 21:38:40 +1000

    Added basic routing functionality

--- flask-server/server.py
+++ flask-server/server.py
@@ -4,7 +4,7 @@ app =  Flask(__name__)
 
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Member3", "Member 4"]}
+    return {"members": ["Member1", "Member2", "Member3"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
